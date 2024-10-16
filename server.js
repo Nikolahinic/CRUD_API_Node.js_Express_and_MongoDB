3bb059a643811b1a7b0d5ce5495bfb7e75c7bccf
Nikola Hinic <nikola.hinic1292@gmail.com>
2024-10-16 20:32:30 +0200

    connect App with database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 
 const app = express();
@@ -6,10 +7,25 @@ const app = express();
 //routes
 app.get('/', (req, res)=>{
     res.send('Hello Node API');
-})
+});
+
+app.get('/blog', (req, res)=>{
+    res.send('Hello Blog');
+});
 
 
-app.listen(3000, ()=>{
-    console.log('Node API app is running on port 3000');
+
+
+mongoose.
+connect('mongodb+srv://admin:123456admin@cluster0.g7kuw.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
     
+    console.log('connected to MongoDB');
+
+    app.listen(3000, ()=>{
+        console.log('Node API app is running on port 3000');
+    });
+    
+}).catch((error)=>{
+    console.log(error);
 });
