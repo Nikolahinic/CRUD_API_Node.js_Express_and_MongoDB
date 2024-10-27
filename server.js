9307358e9092ee22bcfcb8cd5809d5bf0a9c27b6
Nikola Hinic <nikola.hinic1292@gmail.com>
2024-10-27 14:42:26 +0100

    fetch data from database

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModel');
 
 
 const app = express();
+
+
+app.use(express.json());
+
 
 //routes
 app.get('/', (req, res)=>{
@@ -11,6 +16,39 @@ app.get('/', (req, res)=>{
 
 app.get('/blog', (req, res)=>{
     res.send('Hello Blog');
+});
+
+app.get('/products', async(req, res)=>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/products/:id', async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.post('/product', async(req, res)=>{
+    try {
+
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+
+    } catch (error) {
+
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+
+    }
 });
 
 
@@ -25,7 +63,7 @@ connect('mongodb+srv://admin:123456admin@cluster0.g7kuw.mongodb.net/Node-API?ret
     app.listen(3000, ()=>{
         console.log('Node API app is running on port 3000');
     });
-    
+
 }).catch((error)=>{
     console.log(error);
 });
