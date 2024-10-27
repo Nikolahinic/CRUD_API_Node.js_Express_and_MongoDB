eb3176bb8489326968572ac80b65eed9d86d916d
Nikola Hinic <nikola.hinic1292@gmail.com>
2024-10-27 15:27:02 +0100

    delete data from database

--- server.js
+++ server.js
@@ -67,7 +67,23 @@ app.put('/products/:id', async(req, res)=>{
     } catch (error) {
         res.status(500).json({message: error.message});
     }
-})
+});
+
+
+// delete a product
+
+app.delete('/products/:id', async(req, res)=>{
+   try {
+     const {id} = req.params;
+     const product = await Product.findByIdAndDelete(id);
+     if(!product){
+        return res.status(404).json({message: `cannot find any product with ID ${id}`});
+    };
+        res.status(200).json(product);
+   } catch (error) {
+        res.status(500).json({message: error.message});
+   }
+});
 
 
 
